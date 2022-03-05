3365d3df82704bd5bf72cd3bbd3e9543fbc3ad88
TECiNOVA <77143340+tecinova@users.noreply.github.com>
2022-03-05 17:41:18 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,35 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-import os
+#from telegram import Update
+#from telegram.ext import Updater, CommandHandler, CallbackContext
+#import os
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Olá {update.effective_user.first_name}!\nJosemaria aqui 🙋‍♂️ \nSalve Maria Santíssima e viva Cristo Rei.')
+import requests
+from pprint import pprint
+from bs4 import BeautifulSoup
 
-updater = Updater(os.environ.get("TOKEN"), use_context=True)
+ def getMeaning(text):
+        # create url
+        url = 'https://www.oxfordlearnersdictionaries.com/definition/english/' + text
+        # get page
+        page = requests.get(url)
+        # let's soup the page
+        soup = BeautifulSoup(page.text, 'html.parser')
+        pprint(soup)
+	
+	
+	word = 'Mathematics'
+	word = word.strip()
+	getMeaning(word.lower())
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
 
-print("Bot started successfully")
 
-updater.start_polling()
-updater.idle()
+#def start(update: Update, context: CallbackContext) -> None:
+  #  update.message.reply_text(f'Olá, {update.effective_user.first_name}!\nJosemaria aqui 🙋‍♂️ \nSalve Maria Santíssima e viva Cristo Rei.')
+
+#updater = Updater(os.environ.get("TOKEN"), use_context=True)
+
+#updater.dispatcher.add_handler(CommandHandler('start', start))
+
+#print("Bot started successfully")
+
+#updater.start_polling()
+#updater.idle()
