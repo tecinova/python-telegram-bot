1312f57556f28c1ac5acd520dacee117412825f1
TECiNOVA <77143340+tecinova@users.noreply.github.com>
2022-03-05 17:48:10 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,20 +2,36 @@
 #from telegram.ext import Updater, CommandHandler, CallbackContext
 #import os
 
-import requests
-from pprint import pprint
-from bs4 import BeautifulSoup
 
- def getMeaning(text):
-        # create url
-        url = 'https://www.oxfordlearnersdictionaries.com/definition/english/' + text
-        # get page
-        page = requests.get(url)
-        # let's soup the page
-        soup = BeautifulSoup(page.text, 'html.parser')
-        pprint(soup)
-	
-	
+from bs4 import BeautifulSoup
+	import requests
+	from pprint import pprint
+
+	def getMeaning(text):
+	    # create url
+	    url = 'https://www.oxfordlearnersdictionaries.com/definition/english/' + text
+	    # get page
+	    page = requests.get(url)
+	    # let's soup the page
+	    soup = BeautifulSoup(page.text, 'html.parser')
+	    pprint(soup)
+	    try:
+	        # get MP3 and definition
+	        try:
+	            # get MP3
+	            mp3link = soup.find('div', {'class': 'sound audio_play_button pron-uk icon-audio'}).get('data-src-mp3')
+	            pprint(mp3link)
+	        except:
+	            pprint('Pronunciation not found!')
+	        try:
+	            # get definition
+	            definition = soup.find('span', {'class': 'def'}).text
+	            pprint(definition)
+	        except:
+	            pprint('Meaning not found!')
+	    except:
+	        pprint('Something went wrong...')
+
 	word = 'Mathematics'
 	word = word.strip()
 	getMeaning(word.lower())
