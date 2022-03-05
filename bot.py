5dc4af8c755074d16df0e2d5559c964e56b07c07
TECiNOVA <77143340+tecinova@users.noreply.github.com>
2022-03-05 17:55:55 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,31 @@
 #import os
 
 
-from bs4 import BeautifulSoup
-	import requests
+import asyncio
+	import telepot
+	import telepot.aio
+	from telepot.aio.loop import MessageLoop
 	from pprint import pprint
+	from bs4 import BeautifulSoup
+	import requests
+	
+	async def handle(msg):
+	    global chat_id
+	    # These are some useful variables
+	    content_type, chat_type, chat_id = telepot.glance(msg)
+	    # Log variables
+            print(content_type, chat_type, chat_id)
+	    pprint(msg)
+	    username = msg['chat']['first_name']
+	    # Check that the content type is text and not the starting
+	    if content_type == 'text':
+	        if msg['text'] != '/start':
+	            text = msg['text']
+	            # it's better to strip and lower the input
+	            text = text.strip()
+	            await getMeaning(text.lower())
 
-	def getMeaning(text):
+	async def getMeaning(text):
 	    # create url
 	    url = 'https://www.oxfordlearnersdictionaries.com/definition/english/' + text
 	    # get page
@@ -20,21 +40,28 @@ from bs4 import BeautifulSoup
 	        try:
 	            # get MP3
 	            mp3link = soup.find('div', {'class': 'sound audio_play_button pron-uk icon-audio'}).get('data-src-mp3')
-	            pprint(mp3link)
+	            await bot.sendAudio(chat_id=chat_id, audio=mp3link)
 	        except:
-	            pprint('Pronunciation not found!')
+	            await bot.sendMessage(chat_id, 'Pronunciation not found!')
 	        try:
 	            # get definition
 	            definition = soup.find('span', {'class': 'def'}).text
-	            pprint(definition)
+	            await bot.sendMessage(chat_id, definition)
 	        except:
-	            pprint('Meaning not found!')
+	            await bot.sendMessage(chat_id, 'Meaning not found!')
 	    except:
-	        pprint('Something went wrong...')
+	        await bot.sendMessage(chat_id, 'Something went wrong...')
 
-	word = 'Mathematics'
-	word = word.strip()
-	getMeaning(word.lower())
+	# Program startup
+	TOKEN = '2091494140:AAF2W4m3wmWAFo0JX214YJwAWibciVGY9DA'
+	bot = telepot.aio.Bot(TOKEN)
+	loop = asyncio.get_event_loop()
+	loop.create_task(MessageLoop(bot, handle).run_forever())
+	print('Listening ...')
+
+	# Keep the program running
+	loop.run_forever()
+
 
 
 
